80a9406782338866a898638a131fac0d41163bc9
Glitch (mixed-mist) <none>
2018-08-20 05:12:53 +0000

    📟👵 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/timestamp/:date_string?', (req, res) => {
+  let date = new Date()
+  if (req.params.date_string) {
+    try {
+      date = new Date(req.params.date_string)
+    }
+    catch (x) {
+      res.send({ error: 'Invalid Date' })
+      return
+    }
+  }
+  res.send({ unix: date.getTime(), utc: date.toUTCString() })
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
